9df8d7ef27f94cb16cd95097220bad3a92883957
Amadi Chizoba <amadichizobao@gmail.com>
2018-10-30 18:38:38 -0400

    Corrected user model schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
-<<<<<<< HEAD
 const Schema = mongoose.Schema
 const ObjectId = Schema.Types.ObjectId;
 const AddressSchema = require("./subdocuments/address.js");
 const PhoneSchema = require("./subdocuments/phone.js");
-=======
 const Schema = mongoose.Schema;
->>>>>>> c3a8293b4c5f3f6574fd62ef6a32b9f18bd87372
+
 /*
 Schema for User Model
 */
@@ -24,28 +22,26 @@ UserSchema = new Schema({
 	},
 	otherNames: {	//Any other legal names
 		type: String,
-<<<<<<< HEAD
 		lowercase: true,
-	},
-	email: { //unique field
-=======
 		required:false	//Set to false as some indivuals only have first and last names
 	},
 	email: {	//User's email address
->>>>>>> c3a8293b4c5f3f6574fd62ef6a32b9f18bd87372
 		type: String,
 		required: true,
 		unique: true,
 		//add regexp validator
 	},
-<<<<<<< HEAD
 	password: {
 		type:String,
 		required:true,
 		minLength: 8
 	},
 	phone: phoneSchema,
-	electionBodies: [ObjectId],
+	electionBodies: {	//Array of election bodies that user is verified for
+	 	type: [ObjectId],	//Array of ElectionBody id's
+		ref: 'ElectionBody',
+		required: false
+	},
 	demographics: {
 		gender: {
 			type: String,
@@ -83,58 +79,11 @@ UserSchema = new Schema({
 		role : {
 			type: String,
 			enum: ['superuser', 'siteAdmin', 'electionAdmin', 'user'],
-			required: true
+			required: true,
+			default: 'user'
 		}
 })
 
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
 
-
-=======
-	password: {	//User's password(hashed)
-		type: String,
-		required: true
-	},
-	electionBodies: {	//Array of election bodies that user is verified for
-	 	type: [Schema.Types.ObjectId],	//Array of ElectionBody id's
-		ref: 'ElectionBody',
-		required: false
-	},
-	demographics: { //Map of demographic key-string pairs
-		type: Map,
-		of: String
- 	},
-	meta: {
-		isActive: {	//status of user on application
-			type: Boolean,
-			default: true;
-		},
-		role: {		//Role/Privileges of the user
-			type: String,
-			enum: ['superUser', 'siteAdmin', 'electionAdmin', 'user'],
-			default: 'user'
-		},
-		ipAddress: {	//last IP address user access application from
-			type: String
-		},
-		tokenPasscode: {	//Field used for 2fa
-			type: String
-		},
-		dateJoined: {	//Date user joined application
-			type: Date,
-			default: Date.now
-		},
-		lastAccess: {	//date user last access application
-			type: Date
-		}
->>>>>>> c3a8293b4c5f3f6574fd62ef6a32b9f18bd87372
-
-	}
-
-
-<<<<<<< HEAD
-=======
-const User = mongoose.model('User', UserSchema);
-module.exports = User;
->>>>>>> c3a8293b4c5f3f6574fd62ef6a32b9f18bd87372
