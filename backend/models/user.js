b3924311c9f55880a8cdc85dcc481b16bdd5f765
Amadi Chizoba <amadichizobao@gmail.com>
2018-10-30 18:28:19 -0400

    Modification to User Schema. Created Addrees and Phone Schema for use as nested documents.

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 const ObjectId = Schema.Types.ObjectId;
+const AddressSchema = require("./subdocuments/address.js");
+const PhoneSchema = require("./subdocuments/phone.js");
 /*
 Schema for User Model
 */
@@ -8,35 +10,76 @@ Schema for User Model
 UserSchema = new Schema({
 	firstName: {
 		type: String,
-		required: true
+		required: true,
+		lowercase: true,
 	},
 	lastName: {
 		type: String,
-		required: true
+		required: true,
+		lowercase: true,
 	},
 	otherNames: {
 		type: String,
-		required:true
+		lowercase: true,
 	},
-	email: {
+	email: { //unique field
 		type: String,
-		required: true
+		required: true,
+		unique: true,
+		//add regexp validator
 	},
 	password: {
 		type:String,
-		required:true
+		required:true,
+		minLength: 8
 	},
+	phone: phoneSchema,
 	electionBodies: [ObjectId],
-	// demographics: {
+	demographics: {
+		gender: {
+			type: String,
+		},
+		birthDate: {
+			type: Date
+		},
+		address: [AddressSchema],
 
-	// },
-	// meta: {
-
-	// }
-
-
-
+	},
+	meta: {  //Nested object containing other useful information
+		isActive: {
+			type: Boolean,
+			default: true
+		},
+		dateJoined: {
+			type: Date,
+			required: true
+		},
+		lastAccessTime:{
+			type: Date,
+		},
+		lastAccessLocation: {
+			type: String,
+		},
+		ipAddress; {
+			type: String
+		},
+		tokenPasscode: {
+			type: String,
+		},
+		tokenExpiry: {
+			type: Date
+		},
+		role : {
+			type: String,
+			enum: ['superuser', 'siteAdmin', 'electionAdmin', 'user'],
+			required: true
+		}
 })
 
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
+
+
+
+
+
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,42 @@
 let router = require('express').Router(); //router instance to create modular route handlers for user
 let User = require('../models/user');
 
-//list users 
+
+/*
+Login
+*/
+
+
+
+
+
+/*
+Register/Create a user
+*/
+// router.get('/create', (req,res)=>{
+	
+// 	User.create({
+// 		firstName,lastName,otherNames,email,hashedPassword,electionBodies
+// 		,demographics,meta}).then((user))
+// })
+
+/*
+Edit a  user
+*/
+
+
+
+
+/*
+Delete a  user
+*/
+
+
+/*
+
+list users 
+
+*/
 router.get('/list', (req,res)=>{
 	User.find({}).exec().then((users)=>{
 		res.json({users}) //return users from database
@@ -9,5 +44,6 @@ router.get('/list', (req,res)=>{
 		console.log({error: err}) //print errors to console
 	})
 })
+
 
 module.exports = router
